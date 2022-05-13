051efecadb703c6cc1a95c590d9b2875d1af215f
excellence-arch <oladipupomichael9@gmail.com>
2022-05-13 13:39:36 +0100

    integrated webpack

---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+const dotenv = require("dotenv").config({
+  path: path.join(__dirname, "/.env"),
+});
+
+const isDevelopment = process.env.NODE_ENV !== "production";
+
+module.exports = {
+  // mode: "production",
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              ["@babel/preset-react", { runtime: "automatic" }],
+            ],
+            ignore: ["./node_modules/mapbox-gl/dist/mapbox-gl.js"],
+          },
+        },
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|ico)$/,
+        exclude: /node_modules/,
+        use: ["file-loader?name=[name].[ext]"], // ?name=[name].[ext] is only necessary to preserve the original file name
+        // outputPath: "assets/",
+        // publicPath: "/",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jp(e*)g|svg|gif)$/,
+        use: ["file-loader"],
+      },
+      {
+        test: /\.svg$/,
+        use: ["@svgr/webpack"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+      favicon: "./public/favicon.ico",
+      filename: "index.html",
+      manifest: "./public/manifest.json",
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(dotenv.parsed),
+      "process.env.NODE_ENV": JSON.stringify(
+        isDevelopment ? "development" : "production",
+      ),
+    }),
+  ].filter(Boolean),
+};
