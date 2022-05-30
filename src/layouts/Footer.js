3ec1e2bbe7ba6ceb4c7fca9f12d6acda7e728524
excellence-arch <oladipupomichael9@gmail.com>
2022-05-30 12:59:14 +0100

    edited the footer

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -1,8 +1,8 @@
 const Footer = () => {
   return (
-    <div className="text-center">
+    <div className="text-center mb-5">
       <p>&copy; Tripify 2022</p>
-      <p>All Rights Reserved</p>
+      <p className="mb-5 pb-5">All Rights Reserved</p>
     </div>
   );
 };
